e9476499455e58f0ed0e9154262754d2717fd7f6
Add file <18126037@student.hcmus.edu.vn>
2020-08-07 18:52:33 +0700

    Update page

--- js/program.js
+++ js/program.js
@@ -1,3 +1,22 @@
 /*Web shop clothes */
 
 /* JavaScirpt */
+
+/**First Slider */
+$(".slider-one").not("slick-intialized") .slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev", 
+    nextArrow: ".site-slider .slider-btn .next", 
+});
+
+
+/**Second Slider */
+$(".slider-two").not("slick-intialized") .slick({
+    prevArrow: ".site-slider-two .prev", 
+    nextArrow: ".site-slider-two .next", 
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
+});
